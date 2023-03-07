da5da5da066636b53f4de1686d27b5a6519c7df6
FUJITA Tomonori <fujita.tomonori@gmail.com>
2023-03-07 23:45:24 +0900

    rust: add initial netdevice support
    Added minimum abstration APIs for network device drivers.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -22,6 +22,7 @@
 #include <linux/irq.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>
+#include <linux/netdevice.h>
 #include <linux/netfilter_arp.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
